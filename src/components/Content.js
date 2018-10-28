082a25630585bbde4f7ecb9c236414718e72bbc6
Jon <jon.bloomer@iag.com.au>
2018-10-28 15:17:22 +1100

    setup linting, added dummy text, finished scaffold

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import ReactScrollingProgress from './react-scrolling-progress'
+import DummyText from './DummyText'
 
 const Content = () => (
   <main>
+    <ReactScrollingProgress />
     <h1>This is the main content</h1>
+    <DummyText />
   </main>
 )
 
